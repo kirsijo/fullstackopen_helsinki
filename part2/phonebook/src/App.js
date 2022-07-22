fa0b70947fc7a87f729d607994493828bf07b0fc
Kirsi Jokela <kirsijo@gmail.com>
2022-07-22 10:26:15 +0300

    2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,6 +4,7 @@ import Number from "./Number";
 const App = (props) => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [isListed, setIsListed] = useState(false);
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
@@ -11,18 +12,26 @@ const App = (props) => {
   };
 
   const addNumber = (event) => {
-    event.preventDefault();
     const personObject = {
       name: newName,
     };
     setPersons(persons.concat(personObject));
-    console.log(persons);
+  };
+
+  const checkListed = (event) => {
+    event.preventDefault();
+    let result = persons.filter((person) => person.name === newName);
+    if (result.length === 0) {
+      addNumber();
+    } else {
+      alert("this number is already on the list");
+    }
   };
 
   return (
     <>
       <h2>Phonebook</h2>
-      <form onSubmit={addNumber}>
+      <form onSubmit={checkListed}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
